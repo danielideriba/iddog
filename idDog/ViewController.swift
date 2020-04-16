a5765f778845b04f406be8245d7eb0868f82a268
Daniel Ideriba <k2.daniel@grupofleury.com.br>
2020-04-16 15:35:45 -0300

    first commit

---
+++ idDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  idDog
+//
+//  Created by K2 - Daniel Macena Ideriba on 16/04/20.
+//  Copyright © 2020 idDog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ idDogTests/idDogTests.swift
@@ -0,0 +1,34 @@
+//
+//  idDogTests.swift
+//  idDogTests
+//
+//  Created by K2 - Daniel Macena Ideriba on 16/04/20.
+//  Copyright © 2020 idDog. All rights reserved.
+//
+
+import XCTest
+@testable import idDog
+
+class idDogTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
